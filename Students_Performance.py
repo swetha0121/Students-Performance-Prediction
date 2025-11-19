c68433be252a64262e32bd32c82198e39c79096a
Swetha Periasamy <113789540+swetha0121@users.noreply.github.com>
2025-11-19 17:58:38 +0530

    Update Students_Performance.py

--- Students_Performance.py
+++ Students_Performance.py
@@ -368,23 +368,23 @@ with tab5:
     
         fig, ax = plt.subplots(figsize=(6, 2.5)) 
         ax.barh(labels, probabilities, color=['red','green']) 
-        st.pyplot(fig, use_container_width=False)
         for i, v in enumerate(probabilities): 
             ax.text(v + 0.01, i, f"{v:.1%}", va='center')  
         ax.set_xlim(0,1) 
+        st.pyplot(fig, use_container_width=False)
         st.pyplot(fig) 
         
         # Scatter plot
         st.subheader("Student Context")
         fig, ax = plt.subplots(figsize=(6, 2.5))
-        st.pyplot(fig, use_container_width=False)
         ax.scatter(df['Study_Hours_per_Week'], df['Total_Score'], alpha=0.5)
         ax.scatter(study_hours_per_week, total_score, color='red', label="Current Student")
         ax.set_xlabel("Study Hours per Week")
         ax.set_ylabel("Total Score")
         ax.legend()
-
+        st.pyplot(fig, use_container_width=False)
         st.pyplot(fig)
+
 
 
 
